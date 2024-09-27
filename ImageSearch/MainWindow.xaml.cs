4262a90d856ce28410ae6ccbc5bb5cdd91ffe733
netcharm <netcharm@163.com>
2024-09-27 15:46:02 +0800

    Change: remove unused "using" refs

--- ImageSearch/MainWindow.xaml.cs
+++ ImageSearch/MainWindow.xaml.cs
@@ -15,21 +15,14 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Media.TextFormatting;
-using System.Windows.Navigation;
 using System.Windows.Threading;
 
 using CompactExifLib;
 using SkiaSharp;
 using ImageSearch.Search;
-using NumSharp.Utilities;
 
 
 namespace ImageSearch
